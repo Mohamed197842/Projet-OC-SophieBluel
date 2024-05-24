40053bcaf393508155af97d325e36d91a9d6418f
Mohamed Montfort <mohamedmontfort2003@gmail.com>
2024-05-24 21:41:08 +0200

    recuperation des projets et des filtres avec l'api

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,8 +1,54 @@
 
-fetch('http://localhost:5678/api/categories')
-  .then(response=>{
-    return response.json()
-  })
-  .then(json =>{
-    console.log(json)
-  })
+  
+  // Creation des projet
+
+  async function callApiWorks(){
+       //  Affichage des projet 
+    const url = 'http://localhost:5678/api/works'
+    const fetcher = await fetch(url)
+    const json = await fetcher.json();
+    const gallery = document.querySelector("#gallery");
+
+    if (gallery){
+      json.forEach(element => {
+        const imgElement = document.createElement('img')
+        const titreElement = document.createElement('p')
+        imgElement.src = element.imageUrl;
+        titreElement.innerText = element.title
+        
+        gallery.appendChild(imgElement);
+        gallery.appendChild(titreElement)
+        
+      });
+    } else{
+      console.error('Gallery element not found');
+    }
+} 
+
+callApiWorks()
+
+// creation des filtres
+
+async function callApiCategories(){
+  const url = 'http://localhost:5678/api/categories'
+  const fetcher = await fetch(url);
+  const json = await fetcher.json();
+  console.log(json);
+  const filtres = document.querySelector("#filters");
+
+  if (filtres){
+    json.forEach(element=>{
+      const FiltresCategories = document.createElement('button')
+      FiltresCategories.innerText = element.name
+
+      filtres.appendChild(FiltresCategories)
+    });
+  } else{
+    console.error('filtres element not found');
+  }
+}
+
+callApiCategories()
+
+
+  
